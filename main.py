c870c1ff3cb2022e522775c311237ae9d0ae133d
JAYDEEPMISHR <jaydeepmishra1996@gmail.com>
2023-06-25 00:36:57 +0530

    billing_project

--- main.py
+++ main.py
@@ -10,8 +10,21 @@ class Bill_App:
         self.root.geometry("1530x800+0+0")
         self.root.title("Billing System")
 
+        # For image 1
+        img=Image.open("Images/download.jpeg")
+        img=img.resize((500,130),Image.ANTIALIAS)
+        self.photoimg= ImageTk.PhotoImage(img)
 
+        lb1_img=Label(self.root,image=self.photoimg)
+        lb1_img.place(x=0,y=0,width=500,height=130)    
 
+# For image 2
+        img=Image.open("Images/images.jpeg")
+        img=img.resize((500,130),Image.ANTIALIAS)
+        self.photoimg= ImageTk.PhotoImage(img)
+
+        lb1_img=Label(self.root,image=self.photoimg)
+        lb1_img.place(x=0,y=0,width=500,height=130)
 
 
 
