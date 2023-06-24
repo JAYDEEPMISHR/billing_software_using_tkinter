ee8f0c3fe14d90424d7fbdcd94101cfada29d876
JAYDEEPMISHR <jaydeepmishra1996@gmail.com>
2023-06-25 00:20:14 +0530

    tkinter_project

---
+++ main.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from tkinter import ttk
+from PIL import Image,ImageTk  # pip install pillow
+
+
+
+class Bill_App:
+    def __init__(self,root):
+        self.root=root
+        self.root.geometry("1530x800+0+0")
+        self.root.title("Billing System")
+
+
+
+
+
+
+
+
+if __name__ =='__main__':
+    root=Tk()
+    obj=Bill_App(root)
+    root.mainloop()
